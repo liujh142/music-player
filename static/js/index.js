76f03faca09ce16e13d991b011b6445bab6d85d0
xy <1425662757@qq.com>
2021-01-05 13:50:44 +0800

    基本功能实现

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,6 @@
 var audio = new Audio();
 var currentIndex = 0;
 var musicList = [];
-
 function $(selector){
     return document.querySelector(selector);
 }
@@ -26,12 +25,29 @@ function getMusicList(callBack){
 getMusicList(function(list){
     loadMusic(list[currentIndex]);
     musicList = list;
+    generateMusicList(list);
 });
+function generateMusicList(list){
+    var ul = $('.music-list');
+    list.forEach(function(value,index,array){
+        var li = document.createElement('li');
+        var text = document.createTextNode(value.title);
+        li.appendChild(text);
+        ul.appendChild(li);
+    })
+}
 
 function loadMusic(musicObj){
-    audio.src = musicObj.url;
+    audio.src = 'http\:\/\/music\.163\.com\/song\/media\/outer\/url\?id\=' + musicObj.url;
     $('.title').innerText = musicObj.title;
     $('.author').innerText = musicObj.author;
+    // $('.cover').style.backgroundImage = 'url(' + musicObj.background + ')'
+
+}
+audio.onended = function(){
+    currentIndex = (++currentIndex)%musicList.length;
+    loadMusic(musicList[currentIndex]);
+    audio.play();
 }
 
 //music-progess;
@@ -49,11 +65,35 @@ audio.onplay = function(){
         var sec = Math.floor (audio.currentTime % 60) + '';
         sec = sec.length == 2 ? sec : "0" + sec;
         $('.time').innerText = min + ':' + sec;
-    },1000)
+    },1000);
+
+    var li = $('.music-list').querySelectorAll('li')
+    li.forEach(function(value,index){
+        if(index == currentIndex){
+            li[currentIndex].classList.add('active');
+        }else{
+            li[index].classList.remove('active')
+        }
+    })
+
+    var icon = $('.play').querySelector('.iconfont');
+    icon.classList.remove('icon-bofang')
+    icon.classList.add('icon-zanting')
+
 }
 audio.onpause = function(){
     clearInterval(clock);
+
+    var icon = $('.play').querySelector('.iconfont');
+    icon.classList.add('icon-bofang')
+    icon.classList.remove('icon-zanting')
 }
+$('.music-bar').onclick = function(e){
+    var percent = e.offsetX / parseInt(getComputedStyle(this).width);
+    audio.currentTime = audio.duration * percent;
+    audio.play();
+}
+
 
 //music-control
 $('.play').onclick = function(){
@@ -74,12 +114,23 @@ $('.play').onclick = function(){
 $('.forward').onclick = function(){
     currentIndex = (++currentIndex)%musicList.length;
     loadMusic(musicList[currentIndex]);
-    audio.play();
+    audio.play()
 }
 //back
 $('.back').onclick = function(){
     currentIndex = (musicList.length + (--currentIndex))%musicList.length;
     loadMusic(musicList[currentIndex]);
     audio.play();
-
 }
+//music-list
+$('.music-list').addEventListener('click',function(e){
+    var liList =  this.querySelectorAll('li');
+    liList.forEach(function(value,index){
+
+        if(value == e.target){
+            currentIndex = index;
+            loadMusic(musicList[currentIndex]);
+            audio.play();
+        }
+    })
+})
